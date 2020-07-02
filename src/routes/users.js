5979810410837ed99be5adcb1266e250f7e578e8
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-07-02 14:37:36 +0100

    Fix linter errors

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.send('respond with a resource');
 });
 
