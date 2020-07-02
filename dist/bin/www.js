8b431381c6f026e7ca77534dab3a1d6f1d81b523
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-07-02 13:50:54 +0100

    Jest configuration added

--- dist/bin/www.js
+++ dist/bin/www.js
@@ -3,40 +3,44 @@
 /**
  * Module dependencies.
  */
+"use strict";
 
-import app from '../app';
-import debugLib from 'debug';
-import http from 'http';
+var _app = _interopRequireDefault(require("../app"));
 
-const debug = debugLib('business-api:server');
+var _debug = _interopRequireDefault(require("debug"));
 
+var _http = _interopRequireDefault(require("http"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var debug = (0, _debug["default"])('business-api:server');
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || '3000');
 
+_app["default"].set('port', port);
 /**
  * Create HTTP server.
  */
 
-const server = http.createServer(app);
 
+var server = _http["default"].createServer(_app["default"]);
 /**
  * Listen on provided port, on all network interfaces.
  */
 
+
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  const port = parseInt(val, 10);
+  var port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -50,43 +54,40 @@ function normalizePort(val) {
 
   return false;
 }
-
 /**
  * Event listener for HTTP server "error" event.
  */
+
 
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
   }
 
-  const bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port; // handle specific listen errors with friendly messages
 
-  // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
+
     case 'EADDRINUSE':
       console.error(bind + ' is already in use');
       process.exit(1);
       break;
+
     default:
       throw error;
   }
 }
-
 /**
  * Event listener for HTTP server "listening" event.
  */
 
+
 function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
