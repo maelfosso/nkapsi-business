e553cf28c5276a7d5c29d95bb903760cbd3e0b02
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-07-09 16:36:38 +0100

    Business model added

---
+++ src/models/business.js
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+
+const BusinessSchema = new Schema({
+  name: String,
+  email: String,
+  phone: String,
+  
+  description: String,
+  website: String,    
+
+  activities: [{
+    type: String
+  }],
+  delivrables: [{
+    String
+  }]
+});
+
+const Business = model('Business', BusinessSchema);
+
+export default Business;
